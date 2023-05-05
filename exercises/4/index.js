41f72c9f1d8071d0c7e693e85e123cac148510bf
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-05-05 10:32:07 -0400

    help with solution 4

--- exercises/4/index.js
+++ exercises/4/index.js
@@ -1,1 +1,17 @@
-document.querySelectorAll('li').className = "corgitype";
+// document.querySelectorAll('li').className = "corgitype";  incorrect attempt
+
+// Exercise 4: Change the class of the lis to be "corgi" assistance from Curtis Roy
+// const liElements = document.querySelectorAll('li.dog');
+// liElements.forEach(li => {
+    // li.classList.remove('dog');
+    // li.classList.add('corgi');
+// });
+// const liElements = document.querySelectorAll('li');
+// liElements.forEach(li => {
+    // li.classList.replace('dog', 'corgi');
+//});
+
+document.querySelectorAll('li.dog').forEach(li => {
+     li.classList.remove('dog'); 
+     li.classList.add('corgi'); 
+    });
---
+++ exercises/9/index.js
@@ -0,0 +1,4 @@
+const element = document.querySelector('li.corgi');
+element.classList.add('-queen');
+console.log(element.className);
+<div class="corgi-queen"></div>
