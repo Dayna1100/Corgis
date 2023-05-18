15dd5562ed21f0cd1e7401d522e200253008671a
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-05-18 12:13:15 -0400

    update for #9

--- exercises/9/index.js
+++ exercises/9/index.js
@@ -1,4 +1,12 @@
-const element = document.querySelector('li.corgi');
-element.classList.add('-queen');
-console.log(element.className);
-<div class="corgi-queen"></div>
+// My attempt
+// const element = document.querySelector('li.corgi');
+// element.classList.add('-queen');
+// console.log(element.className);
+// <div class="corgi-queen"></div>
+
+//  Help from Curtis
+
+document.querySelectorAll('li.dog').forEach(li => {
+    li.classList.remove('dog');
+    li.classList.add('corgi')
+});
